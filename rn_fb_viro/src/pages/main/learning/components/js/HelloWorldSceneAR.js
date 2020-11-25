018304f2fd187d5f893c6485acebda867e967452
Youngjae Kim <yj122198@naver.com>
2020-11-26 02:13:14 +0900

    final start

--- rn_fb_viro/src/pages/main/learning/components/js/HelloWorldSceneAR.js
+++ rn_fb_viro/src/pages/main/learning/components/js/HelloWorldSceneAR.js
@@ -1,82 +1,90 @@
 'use strict'
 
-import React, { Component } from 'react'
-import { StyleSheet } from 'react-native'
+import React, { Component, useState } from 'react'
+
 import {
   ViroARScene,
   ViroConstants,
   ViroMaterials,
-  ViroImage,
   Viro3DObject,
   ViroAnimations,
-  ViroText,
+  ViroImage,
 } from '@akadrimer/react-viro'
-import storage from '@react-native-firebase/storage'
 
-export default class HelloWorldSceneAR extends Component {
-  constructor() {
-    super()
+const HelloWorldSceneAR = (props) => {
+  // Set initial state here
+  const [text, setText] = useState('Initializing AR...')
+  const [object, setObject] = useState(<></>)
+  const [stage, setStage] = useState(1)
 
-    // Set initial state here
-    this.state = {
-      text: 'Initializing AR...',
-      imageUrl: 'imageUrl',
-      object: <></>,
+  // bind 'this' to functions
+
+  // ViroMaterials.createMaterials({
+  //   wolf: {
+  //     diffuseTexture: require('./res/wolves/Wolves_BaseColor.png'),
+  //   },
+  // });
+
+  const _onInitialized = (state) => {
+    // console.log(ViroConstants.TRACKING_NORMAL)
+
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      setText('Hello World!')
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      //    Handle loss of tracking
     }
-
-    // bind 'this' to functions
-    this._onInitialized = this._onInitialized.bind(this)
-    this.getUrlAndCM()
-    this.styles = StyleSheet.create({
-      Quizstyle: {
-        fontFamily: 'NotoSansCJK',
-        fontSize: 15,
-        fontStyle: 'normal',
-        color: '#000000',
-        textAlignVertical: 'top',
-        textAlign: 'center',
-        fontWeight: 'bold',
-      },
-    })
   }
 
-  render() {
+  ViroMaterials.createMaterials({
+    wolf: {
+      diffuseTexture: require('./res/wolves/Wolves_BaseColor.png'),
+    },
+  })
+
+  ViroAnimations.registerAnimations({
+    bounceUp: {
+      properties: {
+        positionY: '+=0.02',
+      },
+      duration: 500, //.25 seconds..
+    },
+
+    bounceDown: {
+      properties: {
+        positionY: '-=0.02',
+      },
+      duration: 500, //.25 seconds..
+    },
+
+    bounceIt: [['bounceUp', 'bounceDown']],
+    bounceRev: [['bounceDown', 'bounceUp']],
+  })
+
+  if (stage === 1) {
     return (
       <>
-        <ViroARScene onTrackingUpdated={this._onInitialized}>
-          <ViroText
-            text={this.state.text}
-            extrusionDepth={3}
-            scale={[0.5, 0.5, 0.5]}
-            position={[0, 0.2, -0.8]}
-            style={this.styles.Quizstyle}
-          />
-
+        <ViroARScene onTrackingUpdated={_onInitialized}>
           <Viro3DObject
             source={require('./res/wolves/Wolves.obj')}
             materials={['wolf']}
-            position={[-0.4, -0.5, -1.5]}
+            position={[-0.5, -0.5, -1.5]}
             scale={[0.08, 0.08, 0.08]}
             type="OBJ"
             animation={{ name: 'bounceRev', run: true, loop: true }}
             // dragType="FixedToWorld"
             // onDrag={() => {}}
             onClick={() => {
-              this.setState({
-                object: (
-                  <ViroImage
-                    height={0.8}
-                    width={0.8}
-                    position={[0, -0.0, -1.0]}
-                    rotation={[-6, 0, 0]}
-                    source={require('./res/correct.png')}
-                  />
-                ),
-              })
+              setObject(
+                <ViroImage
+                  height={0.8}
+                  width={0.8}
+                  position={[0, -0.0, -1.0]}
+                  rotation={[-6, 0, 0]}
+                  source={require('./res/correct.png')}
+                />,
+              )
               setTimeout(() => {
-                this.setState({
-                  object: <></>,
-                })
+                setObject(<></>)
               }, 5000)
             }}
           />
@@ -89,20 +97,75 @@ export default class HelloWorldSceneAR extends Component {
             scale={[0.08, 0.08, 0.08]}
             type="OBJ"
             onClick={() => {
-              this.setState({
-                object: (
-                  <ViroImage
-                    height={0.8}
-                    width={0.8}
-                    position={[0, -0.0, -1.0]}
-                    source={require('./res/correct.png')}
-                  />
-                ),
-              })
+              setObject(
+                <ViroImage
+                  height={0.8}
+                  width={0.8}
+                  position={[0, -0.0, -1.0]}
+                  rotation={[-6, 0, 0]}
+                  source={require('./res/correct.png')}
+                />,
+              )
               setTimeout(() => {
-                this.setState({
-                  object: <></>,
-                })
+                setObject(<></>)
+                setStage(2)
+              }, 5000)
+            }}
+          />
+          <Viro3DObject
+            name="piano"
+            source={require('./res//wolves/Wolves.obj')}
+            materials={['wolf']}
+            position={[0.5, -0.5, -1.5]}
+            scale={[0.08, 0.08, 0.08]}
+            type="OBJ"
+            animation={{ name: 'bounceRev', run: true, loop: true }}
+            // dragType="FixedToWorld"
+            // onDrag={() => {}}
+            onClick={() => {
+              setObject(
+                <ViroImage
+                  height={0.8}
+                  width={0.8}
+                  position={[0, -0.0, -1.0]}
+                  rotation={[-6, 0, 0]}
+                  source={require('./res/correct.png')}
+                />,
+              )
+              setTimeout(() => {
+                setObject(<></>)
+              }, 5000)
+            }}
+          />
+          {object}
+        </ViroARScene>
+      </>
+    )
+  } else if (stage === 2) {
+    return (
+      <>
+        <ViroARScene onTrackingUpdated={_onInitialized}>
+          <Viro3DObject
+            source={require('./res/wolves/Wolves.obj')}
+            materials={['wolf']}
+            position={[-0.4, -0.5, -1.5]}
+            scale={[0.08, 0.08, 0.08]}
+            type="OBJ"
+            animation={{ name: 'bounceRev', run: true, loop: true }}
+            // dragType="FixedToWorld"
+            // onDrag={() => {}}
+            onClick={() => {
+              setObject(
+                <ViroImage
+                  height={0.8}
+                  width={0.8}
+                  position={[0, -0.0, -1.0]}
+                  rotation={[-6, 0, 0]}
+                  source={require('./res/correct.png')}
+                />,
+              )
+              setTimeout(() => {
+                setObject(<></>)
               }, 5000)
             }}
           />
@@ -117,76 +180,59 @@ export default class HelloWorldSceneAR extends Component {
             // dragType="FixedToWorld"
             // onDrag={() => {}}
             onClick={() => {
-              this.setState({
-                object: (
-                  <ViroImage
-                    height={0.8}
-                    width={0.8}
-                    position={[0, -0.0, -1.0]}
-                    source={require('./res/correct.png')}
-                  />
-                ),
-              })
+              setObject(
+                <ViroImage
+                  height={0.8}
+                  width={0.8}
+                  position={[0, -0.0, -1.0]}
+                  rotation={[-6, 0, 0]}
+                  source={require('./res/correct.png')}
+                />,
+              )
               setTimeout(() => {
-                this.setState({
-                  object: <></>,
-                })
+                setObject(<></>)
+                setStage(3)
               }, 5000)
             }}
           />
-          {this.state.object}
+          {object}
+        </ViroARScene>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <ViroARScene onTrackingUpdated={_onInitialized}>
+          <Viro3DObject
+            source={require('./res/wolves/Wolves.obj')}
+            materials={['wolf']}
+            position={[-0.0, -0.5, -1.5]}
+            scale={[0.08, 0.08, 0.08]}
+            type="OBJ"
+            animation={{ name: 'bounceRev', run: true, loop: true }}
+            // dragType="FixedToWorld"
+            // onDrag={() => {}}
+            onClick={() => {
+              setObject(
+                <ViroImage
+                  height={0.8}
+                  width={0.8}
+                  position={[0, -0.0, -1.0]}
+                  rotation={[-6, 0, 0]}
+                  source={require('./res/correct.png')}
+                />,
+              )
+              setTimeout(() => {
+                setObject(<></>)
+                setStage(1)
+              }, 5000)
+            }}
+          />
+          {object}
         </ViroARScene>
       </>
     )
   }
-
-  _onInitialized(state, reason) {
-    if (state == ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text: '강아지',
-      })
-    } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
-  }
-
-  async getUrlAndCM() {
-    //const url = await storage().ref('Wolves_BaseColor.png').getDownloadURL()
-
-    ViroMaterials.createMaterials({
-      wolf: {
-        diffuseTexture: require('./res/wolves/Wolves_BaseColor.png'),
-      },
-    })
-    ViroAnimations.registerAnimations({
-      bounceUp: {
-        properties: {
-          positionY: '+=0.1',
-        },
-        duration: 500,
-      },
-
-      bounceDown: {
-        properties: {
-          positionY: '-=0.1',
-        },
-        duration: 500,
-      },
-
-      bounceIt: [['bounceDown', 'bounceUp']],
-      bounceRev: [['bounceUp', 'bounceDown']],
-    })
-  }
 }
-
-var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-})
 
 module.exports = HelloWorldSceneAR
